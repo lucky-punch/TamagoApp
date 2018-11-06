69d020787c1393ac390b87234fe560c838ebe4ec
suzutaka <ess.discussion@gmail.com>
2018-11-06 11:55:31 +0900

    TamagoApp

--- TamagoApp/ViewController.swift
+++ TamagoApp/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var topLabel: UILabel!
+    
+    @IBOutlet weak var bottomLabel: UILabel!
+    
+    @IBOutlet weak var topComment: UILabel!
+    
+    var countBox : Int = 10
+    var topcomment : String = "ヨッシーは出てきません"
+    var bottomComment : String = "もう少し！"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func TamagoButton(_ sender: Any) {
+        
+        countBox -= 1
+        topLabel.text = String(countBox)
+        
+        if countBox == 5 {
+            bottomLabel.text = String(bottomComment)
+        }
+        if countBox == 4 {
+            bottomLabel.text = ""
+        }
+        if countBox <= 0 {
+        topLabel.text = ""
+        topComment.text = String(topcomment)
+        }
+    }
+ 
+    
 }
 
